3894eb53f4757f0706e13dc5d605140046d5e2ea
namoclopez2020 <62824677+namoclopez2020@users.noreply.github.com>
2020-09-20 19:41:49 -0500

    hackerman

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,3 @@
 //constantes
 process.env.PORT = process.env.PORT || 3000;
-process.env.urlDB = process.env.urlDB || "mongodb://localhost/tecnek";
+process.env.urlDB = process.env.urlDB || "mongodb://localhost/tecnek"; 
